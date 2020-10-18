a3c4055579039fe7a66e3e57e4088f32565ae99e
Robbert Monna <rjmonna@4cz14502nr.home.monna.org>
2020-10-18 08:25:25 +0200

    React daftpunktocat

--- app/index.js
+++ app/index.js
@@ -4,9 +4,13 @@ import './index.css';
 
 class App extends React.Component{
     render(){
-        return(
-            <div>Hello World</div>
-        )
+        return(<React.Fragment>
+            <img src="https://octodex.github.com/images/daftpunktocat-thomas.gif" id="octocat" alt="octocat-gif" />
+
+            <p>Hello World! I'm rjmonna. This is my website!</p>
+
+            <p>Building websites is fun! You should try it too.</p>
+        </React.Fragment>)
     }
 }
 
