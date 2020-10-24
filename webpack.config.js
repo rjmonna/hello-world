4a2920eaaa81bb61b66c7758f9a80312c114ce69
Robbert Monna <rjmonna@4cz14502nr.home.monna.org>
2020-10-24 15:04:35 +0200

    GH Pages workaround for paths

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,12 @@ module.exports = {
         new HtmlWebpackPlugin ({
             template : 'app/index.html'
         })
-    ]
-
+    ],
+    // to mimic GitHub Pages serving 404.html for all paths
+    // and test spa-github-pages redirect in dev
+    devServer: {
+        historyApiFallback: {
+        rewrites: [{ from: /\//, to: '/404.html' }],
+        },
+    },
 }
