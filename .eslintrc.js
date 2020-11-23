857506c257d14e10b47a5a62c7a63293525c5867
Robbert Monna <rjmonna@4cz14502nr.home.monna.org>
2020-11-23 22:40:15 +0100

    Fixes for configuration and markup

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,5 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
-    },
-    "jshint.enable": false
+    }
 };
--- app/Components/Experiments/ResizableGrid.js
+++ app/Components/Experiments/ResizableGrid.js
@@ -27,32 +27,47 @@ class ResizableGrid extends React.Component{
     }
 
     handleUpdateHeight(e) {
-        const value = e.target.value
+        const value = parseInt(e.target.value)
         
-        if (value) {
+        if (Number.isInteger(value)) {
             this.setState({
-                height: parseInt(value)
+                height: value
             })
         }
     }
 
     handleUpdateWidth(e) {
-        const value = e.target.value
+        const value = parseInt(e.target.value)
         
-        if (value) {
+        if (Number.isInteger(value)) {
             this.setState({
-                width: parseInt(value)
+                width: value
             })
         }
+    }
+
+    handleSaveLocalStorage(e) {
+        const data = this.state.items.data;
+
+        window.localStorage.setItem(this._anEditableListLocalStorageKey, JSON.stringify(data))
+    }
+
+    handleLoadLocalStorage(e) {
+        this.setState({
+            isLoaded: true,
+            items: {
+                data: JSON.parse(window.localStorage.getItem(this._anEditableListLocalStorageKey))
+            }
+        })
     }
 
     render(){
         const { height, width, error, isLoaded } = this.state
 
-        var options = <p>
+        var options = <div>
                         <label htmlFor="height">Height</label><input id="height" defaultValue="1" ref={this.heightRef} onChange={this.handleUpdateHeight.bind(this)}></input><br />
                         <label htmlFor="width">Width</label><input id="width" defaultValue="1" ref={this.widthRef} onChange={this.handleUpdateWidth.bind(this)}></input>
-                    </p>
+                    </div>
 
         if (error) {
             return <div>Error: {error.message}</div>
@@ -61,20 +76,20 @@ class ResizableGrid extends React.Component{
         } else {
             return (
                 <React.Fragment>
+                    <button onClick={this.handleSaveLocalStorage.bind(this)}>Save localStorage</button> <button onClick={this.handleLoadLocalStorage.bind(this)}>Load localStorage</button>
+                    <br/><br/>
                     {options}
-                    <p>
-                        {
-                            [...Array(height).keys()].map(() => {
-                                return(<React.Fragment>
-                                    <div>
-                                        {[...Array(width).keys()].map((item) =>
-                                            <input key={item} defaultValue="a"></input>
-                                        )}
-                                    </div>
-                                </React.Fragment>)
-                            })
-                        }
-                    </p>
+                    {
+                        [...Array(height).keys()].map((rowArray, rowIndex) => {
+                            return(<React.Fragment key={rowIndex}>
+                                <div>
+                                    {[...Array(width).keys()].map((columnArray, columnIndex) =>
+                                        <input key={columnIndex} defaultValue="a"></input>
+                                    )}
+                                </div>
+                            </React.Fragment>)
+                        })
+                    }
                 </React.Fragment>
             )
         }
